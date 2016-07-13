190327ae5df5db227477b95c71d792d49c761340
junjie tang <junjie.tang.1985@gmail.com>
2016-07-13 14:47:30 +0200

    add sync apartment service

---
+++ src/main/java/com/junjie/SyncApartmentService.java
@@ -0,0 +1,25 @@
+package com.junjie;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SyncApartmentService
+{
+
+	public void sync()
+	{
+		//TODO
+		List<String> searchKeywords = Arrays.asList("Steglitz-Steglitz");
+		for (String keyword : searchKeywords)
+		{
+
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
