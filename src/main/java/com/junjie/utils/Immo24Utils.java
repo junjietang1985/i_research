9a3ec0ac1665529703a0efeeda6bf9b3215e676b
junjietang1985 <junjie.tang.1985@gmail.com>
2016-07-13 21:52:07 +0200

    add getPageNumber method for the Area searched

--- src/main/java/com/junjie/utils/Immo24Utils.java
+++ src/main/java/com/junjie/utils/Immo24Utils.java
@@ -1,5 +1,13 @@
 package com.junjie.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+import com.junjie.net.HttpURLConnectionUtils;
+
 public class Immo24Utils
 {
 	public static final String BASE_URL = "https://www.immobilienscout24.de/";
@@ -7,6 +15,8 @@ public class Immo24Utils
 	public static final String PAGE_URL_PRIFIX = "Suche/S-T/P-";
 
 	public static final String PAGE_URL_SUFFIX = "/Wohnung-Miete/Berlin/Berlin/";
+	
+	public static final Pattern COUNT_PAGE_NUM = Pattern.compile("<option.*Wohnung-Miete\\/Berlin\\/Berlin\\/.*");
 
 	/**
 	 * @param page
@@ -19,10 +29,23 @@ public class Immo24Utils
 	{
 		return BASE_URL + PAGE_URL_PRIFIX + page + PAGE_URL_SUFFIX + immoArea;
 	}
+	
+	public static int getPageNumber(String immoArea){
+		String page1Url = getSearchResult(1, immoArea);
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(HttpURLConnectionUtils.getInputStream(page1Url)))){
+
+			return (int) br.lines().filter(line->COUNT_PAGE_NUM.matcher(line).find()).count();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return 1;
+	}
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		System.out.println(getPageNumber("Steglitz-Steglitz"));
+		System.out.println(getPageNumber("Mitte-Mitte"));
+		
 
 	}
 
