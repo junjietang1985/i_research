1d0f9c744cb613b55ff86a1125d4a1ef782066ef
junjietang1985 <junjie.tang.1985@gmail.com>
2016-07-18 21:59:17 +0200

    update numerical utils

--- src/main/java/com/junjie/utils/NumericalUtils.java
+++ src/main/java/com/junjie/utils/NumericalUtils.java
@@ -6,7 +6,7 @@ import java.util.Locale;
 
 public class NumericalUtils
 {
-	public static Float getFloatFrom(String floatStr, Locale locale)
+	public static Float parseFloat(String floatStr, Locale locale)
 	{
 		try
 		{
@@ -20,8 +20,23 @@ public class NumericalUtils
 		}
 		return null;
 	}
+	
+	public static Double parseDouble(String doubleStr, Locale locale)
+	{
+		try
+		{
+			NumberFormat format = NumberFormat.getInstance(locale);
+			Number number = format.parse(doubleStr);
+			return number.doubleValue();
+		}
+		catch (ParseException e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-	public static Integer getIntegerFrom(String intStr, Locale locale)
+	public static Integer parseInt(String intStr, Locale locale)
 	{
 		try
 		{
